03b334ad120a0908b7b74f5c99a21ee049f0d2eb
xuhj1 <xuhj@hzyc.onaliyun.com>
2020-08-27 19:33:41 +0800

    Commit

--- modules/liberty-gorilla-framework-core/src/main/java/com/liberty/gorilla/framework/core/GorillaApplicationStartRun.java
+++ modules/liberty-gorilla-framework-core/src/main/java/com/liberty/gorilla/framework/core/GorillaApplicationStartRun.java
@@ -1,18 +1,27 @@
 package com.liberty.gorilla.framework.core;
 
 
-
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.yaml.snakeyaml.Yaml;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @ComponentScan({"com.liberty.gorilla.framework"})
 @SpringBootApplication()
 public class GorillaApplicationStartRun {
 
-    private static final String[] APPLICATION_CONFIG_FILE_NAME = new String[]{"application.properties","application.yml"};
+    private static final String[] APPLICATION_CONFIG_FILE_NAME = new String[]{"application.properties", "application.yml"};
+
     public GorillaApplicationStartRun() {
     }
 
@@ -21,7 +30,26 @@ public class GorillaApplicationStartRun {
         SpringApplication application = new SpringApplication(GorillaApplicationStartRun.class);
         application.setBannerMode(Banner.Mode.LOG);
         ConfigurableApplicationContext applicationContext = application.run(args);
+        getApplicationProperties();
         System.out.println("**Gorilla***应用:已启动成功!");
+    }
+
+    private static Properties getApplicationProperties() {
+        Properties properties = new Properties();
+        try {
+            ClassPathResource classPathResource = new ClassPathResource("application.properties");
+            InputStream is  = classPathResource.getInputStream();
+            properties.load(is);
+        } catch (IOException e) {
+            System.out.println("未读取到配置文件[application.properties,application.yml]");
+        }
+        return properties;
+    }
+
+    public static Properties properties() {
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new ClassPathResource("application.yml"));
+        return yaml.getObject();
     }
 
 }
