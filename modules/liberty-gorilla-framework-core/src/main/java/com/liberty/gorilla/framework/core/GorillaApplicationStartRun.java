ee71bae305c7b7595dcbe19ec955c20c856dfe4d
xuhj1 <xuhj@hzyc.onaliyun.com>
2020-09-01 10:37:47 +0800

    Commit

--- modules/liberty-gorilla-framework-core/src/main/java/com/liberty/gorilla/framework/core/GorillaApplicationStartRun.java
+++ modules/liberty-gorilla-framework-core/src/main/java/com/liberty/gorilla/framework/core/GorillaApplicationStartRun.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.io.ClassPathResource;
 
 
